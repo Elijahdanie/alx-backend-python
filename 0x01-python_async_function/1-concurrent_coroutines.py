faeb1c2f17625c650a4ee7424b7e9ab82def56ed
Elijah Daniel <thachromatone@gmail.com>
2022-04-18 15:14:45 +0100

    documented

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-
 """
-Docs...
+This module demonstrates concurrent coroutines
 """
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n, max_delay) -> list:
     """
-    Docs ...
+    This function spawns n numbers of coroutines and
+    returns a list of the time taken for each routine
+    in an ascending order
     """
-    final_list:list = []
+    final_list: list = []
     for index in range(n):
-        return_val:float = await wait_random(max_delay)
+        return_val: float = await wait_random(max_delay)
         final_list.append(return_val)
     return final_list
