91aa82f42b88ccd5666ac46895bc438a328d3eed
Elijah Daniel <thachromatone@gmail.com>
2022-04-18 14:06:44 +0100

    changed float to integer

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,7 +8,7 @@ import asyncio
 import random
 
 
-async def wait_random(maxdelay:float=10)->float:
+async def wait_random(maxdelay:int=10)->float:
     """
     This function generates a random time in seconds
     and returns the value after waiting for the supposed time
