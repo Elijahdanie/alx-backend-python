f24d2ecce798e6a0d77900c3519ba6e1d03ab519
Elijah Daniel <thachromatone@gmail.com>
2022-04-19 15:47:47 +0100

    fixed error with return type

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,11 @@
 """
 This module demonstrates async comprehension
 """
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> float:
+async def async_comprehension() -> List[float]:
     """
     This function returns a list of 10 randome
     numbers gotten from async_generator
