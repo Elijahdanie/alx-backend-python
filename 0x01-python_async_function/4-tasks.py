e71493df2c8fddc816d1974fbc46466f52476070
Elijah Daniel <thachromatone@gmail.com>
2022-04-19 14:13:44 +0100

    Added return type

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int):
+async def task_wait_n(n: int, max_delay: int) -> List[asyncio.Task]:
     """
     This function returns list of tasks
     that returns time for running async
