9e5c27995034e78c1b77e58f54456763d7312218
Elijah Daniel <thachromatone@gmail.com>
2022-04-19 15:52:47 +0100

    fixed wrong typing for return type

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,13 +7,13 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[asyncio.Task]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     This function returns list of tasks
     that returns time for running async
     operations.
     """
 
-    final_list: List[asyncio.Task] = await asyncio.gather(
+    final_list: List[float] = await asyncio.gather(
         *[task_wait_random(max_delay) for _ in range(n)])
     return sorted(final_list)
